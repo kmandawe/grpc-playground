b37812e731bb9237bb62f1e2667db12ce0c86e3a
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-05-26 14:27:46 +0800

    Creating instance from generated source

---
+++ src/main/java/com/kensbunker/sec01/SimpleProtoDemo.java
@@ -0,0 +1,14 @@
+package com.kensbunker.sec01;
+
+import com.kensbunker.models.PersonOuterClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleProtoDemo {
+  private static final Logger LOG = LoggerFactory.getLogger(SimpleProtoDemo.class);
+
+  public static void main(String[] args) {
+    var person = PersonOuterClass.Person.newBuilder().setName("Sam").setAge(12).build();
+    LOG.info("Person: {}", person);
+  }
+}
