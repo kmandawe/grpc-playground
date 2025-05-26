86919a6d587fdece1fcace1ff7ea119049313380
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-05-26 15:22:56 +0800

    Serialization & Deserialization

---
+++ src/main/java/com/kensbunker/sec03/Lec01Serialization.java
@@ -0,0 +1,38 @@
+package com.kensbunker.sec03;
+
+import com.kensbunker.models.sec03.Person;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Lec01Serialization {
+  private static final Logger LOG = LoggerFactory.getLogger(Lec01Serialization.class);
+  private static final Path PATH = Path.of("person.out");
+
+  public static void main(String[] args) throws IOException {
+    var person =
+        Person.newBuilder()
+            .setLastName("sam")
+            .setAge(12)
+            .setEmail("sam@gmail.com")
+            .setEmployed(true)
+            .setSalary(1000.2345)
+            .setBankAccountNumber(123456789012L)
+            .setBalance(-10000)
+            .build();
+    serialize(person);
+    LOG.info("{}", deserialize());
+    LOG.info("equals: {}", person.equals(deserialize()));
+    LOG.info("bytes length: {}", person.toByteArray().length);
+  }
+
+  public static void serialize(Person person) throws IOException {
+    person.writeTo(Files.newOutputStream(PATH));
+  }
+
+  public static Person deserialize() throws IOException {
+    return Person.parseFrom(Files.newInputStream(PATH));
+  }
+}
