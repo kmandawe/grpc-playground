649c6d6e4c3e95dc341252416869959d6d157469
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-05-27 16:02:42 +0800

    Abstract Test

---
+++ src/test/java/com/kensbunker/test/sec06/AbstractTest.java
@@ -0,0 +1,25 @@
+package com.kensbunker.test.sec06;
+
+import com.kensbunker.common.GrpcServer;
+import com.kensbunker.models.sec06.BankServiceGrpc;
+import com.kensbunker.models.sec06.BankServiceGrpc.BankServiceBlockingStub;
+import com.kensbunker.sec06.BankService;
+import com.kensbunker.test.common.AbstractChannelTest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+public abstract class AbstractTest extends AbstractChannelTest {
+  private final GrpcServer grpcServer = GrpcServer.create(new BankService());
+  protected BankServiceGrpc.BankServiceBlockingStub blockingStub;
+
+  @BeforeAll
+  public void setup() {
+    this.grpcServer.start();
+    this.blockingStub = BankServiceGrpc.newBlockingStub(channel);
+  }
+
+  @AfterAll
+  public void stop() {
+    this.grpcServer.stop();
+  }
+}
