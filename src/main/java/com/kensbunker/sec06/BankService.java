7629392395849636cd65aebba158c0f954a57d80
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-05-27 12:47:20 +0800

    Account Repository

--- src/main/java/com/kensbunker/sec06/BankService.java
+++ src/main/java/com/kensbunker/sec06/BankService.java
@@ -3,6 +3,7 @@ package com.kensbunker.sec06;
 import com.kensbunker.models.sec06.AccountBalance;
 import com.kensbunker.models.sec06.BalanceCheckRequest;
 import com.kensbunker.models.sec06.BankServiceGrpc;
+import com.kensbunker.sec06.repository.AccountRepository;
 import io.grpc.stub.StreamObserver;
 
 public class BankService extends BankServiceGrpc.BankServiceImplBase {
@@ -11,8 +12,9 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
   public void getAccountBalance(BalanceCheckRequest request,
       StreamObserver<AccountBalance> responseObserver) {
     var accountNumber = request.getAccountNumber();
+    var balance = AccountRepository.getBalance(accountNumber);
     var accountBalance = AccountBalance.newBuilder().setAccountNumber(accountNumber)
-        .setBalance(accountNumber * 10).build();
+        .setBalance(balance).build();
 
     responseObserver.onNext(accountBalance);
     responseObserver.onCompleted();
---
+++ src/main/java/com/kensbunker/sec06/repository/AccountRepository.java
@@ -0,0 +1,18 @@
+package com.kensbunker.sec06.repository;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class AccountRepository {
+  public static final Map<Integer, Integer> DB =
+      IntStream.rangeClosed(1, 10)
+          .boxed()
+          .collect(Collectors.toConcurrentMap(Function.identity(), v -> 100));
+
+
+  public static Integer getBalance(int accountNumber) {
+    return DB.get(accountNumber);
+  }
+}
