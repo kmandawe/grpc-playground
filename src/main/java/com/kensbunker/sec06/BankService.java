48a19545e81ea102877454e35c2f9a280f7bda8e
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-05-28 17:36:51 +0800

    Server Streaming RPC implementation

--- src/main/java/com/kensbunker/sec06/BankService.java
+++ src/main/java/com/kensbunker/sec06/BankService.java
@@ -1,14 +1,22 @@
 package com.kensbunker.sec06;
 
+import com.google.common.util.concurrent.Uninterruptibles;
 import com.google.protobuf.Empty;
 import com.kensbunker.models.sec06.AccountBalance;
 import com.kensbunker.models.sec06.AllAccountsResponse;
 import com.kensbunker.models.sec06.BalanceCheckRequest;
 import com.kensbunker.models.sec06.BankServiceGrpc;
+import com.kensbunker.models.sec06.Money;
+import com.kensbunker.models.sec06.WithdrawRequest;
 import com.kensbunker.sec06.repository.AccountRepository;
 import io.grpc.stub.StreamObserver;
+import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BankService extends BankServiceGrpc.BankServiceImplBase {
+
+  private static final Logger LOG = LoggerFactory.getLogger(BankService.class);
 
   @Override
   public void getAccountBalance(BalanceCheckRequest request,
@@ -32,6 +40,30 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
 
     var response = AllAccountsResponse.newBuilder().addAllAccounts(accounts).build();
     responseObserver.onNext(response);
+    responseObserver.onCompleted();
+  }
+
+  @Override
+  public void withdraw(WithdrawRequest request, StreamObserver<Money> responseObserver) {
+    /*
+        Assumption: account # 1 - 10 & withdraw amount is multiple of $10
+     */
+    var accountNumber = request.getAccountNumber();
+    var requestedAmount = request.getAmount();
+    var accountBalance = AccountRepository.getBalance(accountNumber);
+
+    if (requestedAmount > accountBalance) {
+      responseObserver.onCompleted(); // change to proper error later
+      return;
+    }
+
+    for(int i = 0; i < requestedAmount / 10; i++) {
+      var money = Money.newBuilder().setAmount(10).build();
+      responseObserver.onNext(money);
+      LOG.info("money sent {}", money);
+      AccountRepository.decuctAmount(accountNumber, 10);
+      Uninterruptibles.sleepUninterruptibly(1, TimeUnit.SECONDS);
+    }
     responseObserver.onCompleted();
   }
 }
