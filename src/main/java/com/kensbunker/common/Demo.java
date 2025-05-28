a2a52caa8c901feb0242dba12b6f2f7d20bd177c
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-05-28 20:18:07 +0800

    Demo via Postman

--- src/main/java/com/kensbunker/common/Demo.java
+++ src/main/java/com/kensbunker/common/Demo.java
@@ -1,10 +1,10 @@
 package com.kensbunker.common;
 
 import com.kensbunker.sec06.BankService;
+import com.kensbunker.sec06.TransferService;
 
 public class Demo {
-  public static void main(String[] args){
-    GrpcServer.create(new BankService())
-        .start().await();
+  public static void main(String[] args) {
+    GrpcServer.create(new BankService(), new TransferService()).start().await();
   }
 }
--- src/main/java/com/kensbunker/sec06/TransferService.java
+++ src/main/java/com/kensbunker/sec06/TransferService.java
@@ -3,6 +3,7 @@ package com.kensbunker.sec06;
 import com.kensbunker.models.sec06.TransferRequest;
 import com.kensbunker.models.sec06.TransferResponse;
 import com.kensbunker.models.sec06.TransferServiceGrpc.TransferServiceImplBase;
+import com.kensbunker.sec06.requesthandlers.TransferRequestHandler;
 import io.grpc.stub.StreamObserver;
 
 public class TransferService extends TransferServiceImplBase {
@@ -10,6 +11,6 @@ public class TransferService extends TransferServiceImplBase {
   @Override
   public StreamObserver<TransferRequest> transfer(
       StreamObserver<TransferResponse> responseObserver) {
-
+    return new TransferRequestHandler(responseObserver);
   }
 }
