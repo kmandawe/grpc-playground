70ecedbe11853df3719007786eaa2bade6cc0c81
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-05-28 21:09:19 +0800

    Flow Control Demo via Postman

--- src/main/java/com/kensbunker/common/Demo.java
+++ src/main/java/com/kensbunker/common/Demo.java
@@ -2,9 +2,10 @@ package com.kensbunker.common;
 
 import com.kensbunker.sec06.BankService;
 import com.kensbunker.sec06.TransferService;
+import com.kensbunker.sec07.FlowControlService;
 
 public class Demo {
   public static void main(String[] args) {
-    GrpcServer.create(new BankService(), new TransferService()).start().await();
+    GrpcServer.create(new FlowControlService()).start().await();
   }
 }
