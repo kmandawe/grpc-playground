5843656dfb939cbffbe29d96c24143884dc33842
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-05-29 09:55:22 +0800

    Server Error

--- src/main/java/com/kensbunker/sec09/BankService.java
+++ src/main/java/com/kensbunker/sec09/BankService.java
@@ -16,22 +16,20 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
   private static final Logger log = LoggerFactory.getLogger(BankService.class);
 
   @Override
-  public void getAccountBalance(BalanceCheckRequest request, StreamObserver<AccountBalance> responseObserver) {
+  public void getAccountBalance(
+      BalanceCheckRequest request, StreamObserver<AccountBalance> responseObserver) {
     RequestValidator.validateAccount(request.getAccountNumber())
         .map(Status::asRuntimeException)
         .ifPresentOrElse(
-            responseObserver::onError,
-            () -> sendAccountBalance(request, responseObserver)
-        );
+            responseObserver::onError, () -> sendAccountBalance(request, responseObserver));
   }
 
-  private void sendAccountBalance(BalanceCheckRequest request, StreamObserver<AccountBalance> responseObserver) {
+  private void sendAccountBalance(
+      BalanceCheckRequest request, StreamObserver<AccountBalance> responseObserver) {
     var accountNumber = request.getAccountNumber();
     var balance = AccountRepository.getBalance(accountNumber);
-    var accountBalance = AccountBalance.newBuilder()
-        .setAccountNumber(accountNumber)
-        .setBalance(balance)
-        .build();
+    var accountBalance =
+        AccountBalance.newBuilder().setAccountNumber(accountNumber).setBalance(balance).build();
     responseObserver.onNext(accountBalance);
     responseObserver.onCompleted();
   }
@@ -40,25 +38,32 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
   public void withdraw(WithdrawRequest request, StreamObserver<Money> responseObserver) {
     RequestValidator.validateAccount(request.getAccountNumber())
         .or(() -> RequestValidator.isAmountDivisibleBy10(request.getAmount()))
-        .or(() -> RequestValidator.hasSufficientBalance(request.getAmount(), AccountRepository.getBalance(request.getAccountNumber())))
+        .or(
+            () ->
+                RequestValidator.hasSufficientBalance(
+                    request.getAmount(), AccountRepository.getBalance(request.getAccountNumber())))
         .map(Status::asRuntimeException)
-        .ifPresentOrElse(
-            responseObserver::onError,
-            () -> sendMoney(request, responseObserver)
-        );
+        .ifPresentOrElse(responseObserver::onError, () -> sendMoney(request, responseObserver));
   }
 
   private void sendMoney(WithdrawRequest request, StreamObserver<Money> responseObserver) {
-    var accountNumber = request.getAccountNumber();
-    var requestedAmount = request.getAmount();
-    for (int i = 0; i < (requestedAmount / 10); i++) {
-      var money = Money.newBuilder().setAmount(10).build();
-      responseObserver.onNext(money);
-      log.info("money sent {}", money);
-      AccountRepository.deductAmount(accountNumber, 10);
-      Uninterruptibles.sleepUninterruptibly(1, TimeUnit.SECONDS);
+    try {
+      var accountNumber = request.getAccountNumber();
+      var requestedAmount = request.getAmount();
+      for (int i = 0; i < (requestedAmount / 10); i++) {
+        var money = Money.newBuilder().setAmount(10).build();
+        if (i == 3) {
+          throw new RuntimeException("oops");
+        }
+        responseObserver.onNext(money);
+        log.info("money sent {}", money);
+        AccountRepository.deductAmount(accountNumber, 10);
+        Uninterruptibles.sleepUninterruptibly(1, TimeUnit.SECONDS);
+      }
+      responseObserver.onCompleted();
+    } catch (Exception e) {
+      responseObserver.onError(
+          Status.INTERNAL.withDescription(e.getMessage()).asRuntimeException());
     }
-    responseObserver.onCompleted();
   }
-
 }
