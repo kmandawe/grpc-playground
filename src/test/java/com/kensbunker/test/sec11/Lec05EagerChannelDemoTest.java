7f9a7e7438121c264f0524f9a9dc9dd87779b7af
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-05-30 21:39:10 +0800

    Eager Channel Creation

--- src/test/java/com/kensbunker/test/sec11/Lec05EagerChannelDemoTest.java
+++ src/test/java/com/kensbunker/test/sec11/Lec05EagerChannelDemoTest.java
@@ -4,10 +4,8 @@ import com.google.common.util.concurrent.Uninterruptibles;
 import com.kensbunker.common.GrpcServer;
 import com.kensbunker.models.sec11.BalanceCheckRequest;
 import com.kensbunker.models.sec11.BankServiceGrpc;
-import com.kensbunker.models.sec11.WithdrawRequest;
 import com.kensbunker.sec11.DeadlineBankService;
 import com.kensbunker.test.common.AbstractChannelTest;
-import io.grpc.Deadline;
 import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -15,8 +13,8 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Lec04LazyChannelDemoTest extends AbstractChannelTest {
-  private static final Logger LOG = LoggerFactory.getLogger(Lec04LazyChannelDemoTest.class);
+public class Lec05EagerChannelDemoTest extends AbstractChannelTest {
+  private static final Logger LOG = LoggerFactory.getLogger(Lec05EagerChannelDemoTest.class);
 
   private final GrpcServer grpcServer = GrpcServer.create(new DeadlineBankService());
   private BankServiceGrpc.BankServiceBlockingStub bankBlockingStub;
@@ -28,11 +26,8 @@ public class Lec04LazyChannelDemoTest extends AbstractChannelTest {
   }
 
   @Test
-  public void lazyChannelDemo() {
-    var request = BalanceCheckRequest.newBuilder().setAccountNumber(1).build();
-    Uninterruptibles.sleepUninterruptibly(3, TimeUnit.SECONDS);
-    var response = this.bankBlockingStub.getAccountBalance(request);
-    LOG.info("{}", response);
+  public void eagerChannelDemo() {
+    LOG.info("{}", channel.getState(true));
   }
 
   @AfterAll
