8afca58462503267127c19de355d68f49f20441a
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-05-30 21:36:04 +0800

    Lazy Channel Creation

--- src/test/java/com/kensbunker/test/sec11/Lec04LazyChannelDemoTest.java
+++ src/test/java/com/kensbunker/test/sec11/Lec04LazyChannelDemoTest.java
@@ -2,6 +2,7 @@ package com.kensbunker.test.sec11;
 
 import com.google.common.util.concurrent.Uninterruptibles;
 import com.kensbunker.common.GrpcServer;
+import com.kensbunker.models.sec11.BalanceCheckRequest;
 import com.kensbunker.models.sec11.BankServiceGrpc;
 import com.kensbunker.models.sec11.WithdrawRequest;
 import com.kensbunker.sec11.DeadlineBankService;
@@ -14,36 +15,24 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Lec03WaitForReadyTest extends AbstractChannelTest {
-  private static final Logger LOG = LoggerFactory.getLogger(Lec03WaitForReadyTest.class);
+public class Lec04LazyChannelDemoTest extends AbstractChannelTest {
+  private static final Logger LOG = LoggerFactory.getLogger(Lec04LazyChannelDemoTest.class);
 
   private final GrpcServer grpcServer = GrpcServer.create(new DeadlineBankService());
   private BankServiceGrpc.BankServiceBlockingStub bankBlockingStub;
 
   @BeforeAll
   public void setup() {
-    //    this.grpcServer.start();
-    Runnable runnable =
-        () -> {
-          Uninterruptibles.sleepUninterruptibly(5, TimeUnit.SECONDS);
-          this.grpcServer.start();
-        };
-    Thread.ofVirtual().start(runnable);
+//    this.grpcServer.start();
     this.bankBlockingStub = BankServiceGrpc.newBlockingStub(channel);
   }
 
   @Test
-  public void blockingDeadlineTest() {
-    LOG.info("sending the request");
-    var request = WithdrawRequest.newBuilder().setAccountNumber(1).setAmount(50).build();
-    var iterator =
-        this.bankBlockingStub
-            .withWaitForReady()
-            .withDeadline(Deadline.after(15, TimeUnit.SECONDS))
-            .withdraw(request);
-    while (iterator.hasNext()) {
-      LOG.info("{}", iterator.next());
-    }
+  public void lazyChannelDemo() {
+    var request = BalanceCheckRequest.newBuilder().setAccountNumber(1).build();
+    Uninterruptibles.sleepUninterruptibly(3, TimeUnit.SECONDS);
+    var response = this.bankBlockingStub.getAccountBalance(request);
+    LOG.info("{}", response);
   }
 
   @AfterAll
