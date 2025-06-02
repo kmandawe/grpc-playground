eda987cce3c14f8ab2725a4170738b282de072d6
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-06-02 09:53:37 +0800

    Multiple Interceptors

--- src/test/java/com/kensbunker/test/sec12/Lec04GzipInterceptorTest.java
+++ src/test/java/com/kensbunker/test/sec12/Lec04GzipInterceptorTest.java
@@ -1,16 +1,19 @@
 package com.kensbunker.test.sec12;
 
 import com.kensbunker.models.sec12.BalanceCheckRequest;
+import com.kensbunker.test.sec12.interceptors.DeadlineInterceptor;
 import com.kensbunker.test.sec12.interceptors.GzipRequestInterceptor;
 import io.grpc.ClientInterceptor;
+import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
 
 public class Lec04GzipInterceptorTest extends AbstractInterceptorTest {
 
   @Override
   protected List<ClientInterceptor> getClientInterceptors() {
-    return List.of(new GzipRequestInterceptor());
+    return List.of(new GzipRequestInterceptor(), new DeadlineInterceptor(Duration.ofSeconds(2)));
   }
 
   @Test
