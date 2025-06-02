80b365647c04035b69e761dfb9e899b77e3423ac
kmandawe <kenneth.mandawe@cheetahdigital.com>
2025-06-02 09:51:03 +0800

    Compression via Interceptor

---
+++ src/test/java/com/kensbunker/test/sec12/Lec04GzipInterceptorTest.java
@@ -0,0 +1,21 @@
+package com.kensbunker.test.sec12;
+
+import com.kensbunker.models.sec12.BalanceCheckRequest;
+import com.kensbunker.test.sec12.interceptors.GzipRequestInterceptor;
+import io.grpc.ClientInterceptor;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class Lec04GzipInterceptorTest extends AbstractInterceptorTest {
+
+  @Override
+  protected List<ClientInterceptor> getClientInterceptors() {
+    return List.of(new GzipRequestInterceptor());
+  }
+
+  @Test
+  public void gzipDemo() {
+    var request = BalanceCheckRequest.newBuilder().setAccountNumber(1).build();
+    var response = this.bankBlockingStub.getAccountBalance(request);
+  }
+}
---
+++ src/test/java/com/kensbunker/test/sec12/interceptors/GzipRequestInterceptor.java
@@ -0,0 +1,20 @@
+package com.kensbunker.test.sec12.interceptors;
+
+import io.grpc.CallOptions;
+import io.grpc.Channel;
+import io.grpc.ClientCall;
+import io.grpc.ClientInterceptor;
+import io.grpc.MethodDescriptor;
+
+public class GzipRequestInterceptor implements ClientInterceptor {
+
+
+  @Override
+  public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(
+      MethodDescriptor<ReqT, RespT> methodDescriptor, CallOptions callOptions, Channel channel) {
+    return channel.newCall(methodDescriptor, callOptions.withCompression("gzip"));
+  }
+
+
+}
+
